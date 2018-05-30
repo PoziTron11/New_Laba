faa5e71c32332913b70ae25acd395ba0fe2d66dc
PoziTron11 <perchina.xenia@gmail.com>
2018-05-30 20:11:15 +0700

    first project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static double sum(double a, double b)
+        {
+            return a + b;
+
+        }
+        public static double umnoz(double a, double b)
+        {
+            return a * b;
+
+        }
+        public static double vicit(double a, double b)
+        {
+            return a - b;
+
+        }
+        public static double del(double a, double b)
+        {
+            return a / b;
+
+        }
+
+
+
+
+    }
+
+}
